ad39a74440205bd546348362015dbdab1501d947
Dian Pan <dian.di.pan@gmail.com>
2016-03-08 21:32:51 -0800

    Add Elm starter files
    -also includes querying for repositories written in Elm on Github

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# try-elm-
-find repos written in Elm from Github
+#try-elm
+#### getting a taste of Elm per [rtfeldman's workshop](https://github.com/rtfeldman/elm-workshop)
+
+## Installation
+
+```bash
+npm install
+```
+
+## Engage Auto-Rebuilding
+
+```bash
+npm run watch
+```
---
+++ src/ElmHub.elm
@@ -0,0 +1,176 @@
+module ElmHub (..) where
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import StartApp
+import Http
+import Task exposing (Task)
+import Effects exposing (Effects)
+import Json.Decode exposing (Decoder, (:=))
+import Json.Encode
+import Signal exposing (Address)
+
+
+main : Signal Html
+main =
+  app.html
+
+
+app : StartApp.App Model
+app =
+  StartApp.start
+    { view = view
+    , update = update
+    , init = ( initialModel, Effects.task (searchFeed initialModel.query) )
+    , inputs = []
+    }
+
+
+port tasks : Signal (Task Effects.Never ())
+port tasks =
+  app.tasks
+
+
+searchFeed : String -> Task x Action
+searchFeed query =
+  let
+    -- See https://developer.github.com/v3/search/#example for how to customize!
+    url =
+      "https://api.github.com/search/repositories?q="
+        ++ query
+        ++ "+language:elm&sort=stars&order=desc"
+
+    task : Task Http.Error (List SearchResult)
+    task =
+      Http.get responseDecoder url
+
+    actionyTask : Task Http.Error Action
+    actionyTask =
+      Task.map SetResults task
+
+    errorProofTask : Task x Action
+    errorProofTask =
+      Task.onError
+        actionyTask
+        (\_ -> Task.succeed (SetResults []))
+
+    --|> Task.map SetResults
+  in
+    errorProofTask
+
+
+responseDecoder : Decoder (List SearchResult)
+responseDecoder =
+  "items" := Json.Decode.list searchResultDecoder
+
+
+searchResultDecoder : Decoder SearchResult
+searchResultDecoder =
+  Json.Decode.object3
+    SearchResult
+    -- See https://developer.github.com/v3/search/#example
+    ("id" := Json.Decode.int)
+    ("full_name" := Json.Decode.string)
+    ("stargazers_count" := Json.Decode.int)
+
+
+type alias Model =
+  { query : String
+  , results : List SearchResult
+  }
+
+
+type alias SearchResult =
+  { id : ResultId
+  , name : String
+  , stars : Int
+  }
+
+
+type alias ResultId =
+  Int
+
+
+initialModel : Model
+initialModel =
+  { query = "tutorial"
+  , results = []
+  }
+
+
+view : Address Action -> Model -> Html
+view address model =
+  div
+    [ class "content" ]
+    [ header
+        []
+        [ h1 [] [ text "ElmHub" ]
+        , span [ class "tagline" ] [ text "“Like GitHub, but for Elm things.”" ]
+        ]
+    , input [ class "search-query", onInput address SetQuery, defaultValue model.query ] []
+    , button [ class "search-button", onClick address Search ] [ text "Search" ]
+    , ul
+        [ class "results" ]
+        (List.map (viewSearchResult address) model.results)
+    ]
+
+
+onInput address wrap =
+  on "input" targetValue (\val -> Signal.message address (wrap val))
+
+
+defaultValue str =
+  property "defaultValue" (Json.Encode.string str)
+
+
+viewSearchResult : Address Action -> SearchResult -> Html
+viewSearchResult address result =
+  li
+    []
+    [ span [ class "star-count" ] [ text (toString result.stars) ]
+    , a
+        [ href ("https://github.com/" ++ result.name)
+        , class "result-name"
+        , target "_blank"
+        ]
+        [ text result.name ]
+    , button
+        [ class "hide-result", onClick address (HideById result.id) ]
+        [ text "X" ]
+    ]
+
+
+type Action
+  = Search
+  | SetQuery String
+  | HideById ResultId
+  | SetResults (List SearchResult)
+
+
+update : Action -> Model -> ( Model, Effects Action )
+update action model =
+  case action of
+    Search ->
+      ( model, Effects.task (searchFeed model.query) )
+
+    SetQuery query ->
+      ( { model | query = query }, Effects.none )
+
+    SetResults results ->
+      let
+        newModel =
+          { model | results = results }
+      in
+        ( newModel, Effects.none )
+
+    HideById idToHide ->
+      let
+        newResults =
+          model.results
+            |> List.filter (\{ id } -> id /= idToHide)
+
+        newModel =
+          { model | results = newResults }
+      in
+        ( newModel, Effects.none )
